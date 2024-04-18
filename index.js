5576644b23d681434e19e31543ef8cabf0db3807
M. Abdul Rahman <mabdulr44@gmail.com>
2024-04-18 15:37:30 +0700

    conn mongodb

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/Database.js";
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 
+connectDB();
+
 app.listen(process.env.APP_PORT, () =>
-  console.log(`Server up and running on port ${process.env.APP_PORT}`)
+  console.log(`Server up and running on port ${process.env.APP_PORT}...`)
 );
