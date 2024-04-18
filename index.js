c8f117d3a19d80cf44c60cccc17303460896f7cb
M. Abdul Rahman <mabdulr44@gmail.com>
2024-04-18 15:42:34 +0700

    setup express-session

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "./config/Database.js";
+import cors from "cors";
+import session from "express-session";
+import connectDB from "./configs/Database.js";
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use(session({
+    secret: process.env.APP_SESSION,
+    resave: false,
+    saveUninitialized: true,
+}));
 
 connectDB();
 
